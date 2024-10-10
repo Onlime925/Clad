12a3bd9977a1fe1c57e6cf4dd910217f18ff8628
iamowaisazam <iamowaisazam@gmail.com>
2024-10-11 00:45:56 +0500

    update

---
+++ chunk-VDM3L5UN.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var n=[{path:"working",loadComponent:()=>import("./chunk-3CLVFUTW.js").then(o=>o.WorkingComponent)},{path:"agrement",loadComponent:()=>import("./chunk-KSS4KBN3.js").then(o=>o.AgrementComponent)}];export{n as routes};
